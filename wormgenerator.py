f6ea2d40096e73cb129e293102686e8567ad4b2a
therats-collab <linuxfan413@pm.me>
2020-02-02 01:16:53 +1030

    Rewrote file handling; needs to be cleaned up

--- wormgenerator.py
+++ wormgenerator.py
@@ -10,35 +10,18 @@
 # Naming scheme defaults to baseX00.png / eyesX00.png, etc.
 # === Imports === #
 
-from PIL import Image # Lets us do cool stuff with our worm images
-import random         # Lets us randomise worm parts
-import glob           # Lets us do cool file stuff
-import os             # Lets us do cool file stuff part 2
-
+from PIL import Image       # Lets us do cool stuff with our worm images
+from pathlib import Path    # Also does file stuff
+import random               # Lets us randomise worm parts
+import os
 # === Variables === #
-baseACount = 0
-baseBCount = 0
-for name in glob.glob('baseA*.png'):            # Figures out how many "A" bases there are
-    baseACount = baseACount + 1       
-for name in glob.glob('baseB*.png'):            # Figures out how many "B" bases there are
-    baseBCount = baseBCount + 1       
-baseCount = (baseACount + baseBCount)           # Figures out total number of bases
-
-eyeACount = 0
-eyeBCount = 0
-for name in glob.glob('eyesA*.png'):            # Figures out how many "A" eyes there are
-    eyeACount = eyeACount + 1         
-for name in glob.glob('eyesB*.png'):            # Figures out how many "B" eyes there are
-    eyeBCount = eyeBCount + 1         
-eyeCount = (eyeACount + eyeBCount)              # Figures out total number of eyes
-
-patternACount = 0
-patternBCount = 0
-for name in glob.glob('patternA*.png'):         # Figures out how many "A" patterns there are
-    patternACount = patternACount + 1         
-for name in glob.glob('patternB*.png'):         # Figures out how many "B" patterns there are
-    patternBCount = patternBCount + 1         
-patternCount = (patternACount + patternBCount)  # Figures out total number of patterns
+basePath = "input/base/"
+eyePath = "input/eye/"
+patternPath = "input/pattern/"
+baseCount = 24
+eyeCount = 12
+patternCount = 4
+variantNumber = 0
 
 wormsMade = 0
 createdWorms = [""]
@@ -54,15 +37,15 @@ while not done:
 
     while wormsMade < wormQuantity:                         # Does this over and over again, until it's made as many as you asked for
         print("\n[INFO] Starting new worm loop. " + str(wormsMade) + " worms made so far.")
-        baseNum = random.randint(1, (baseCount))            # Picks a random base from all of them
+        variantNumber = random.randint(1, 2)            # Picks a random base from all of them
         # ======== Base A ======== #
-        if baseNum <= baseACount:                           # Treats base A as the starting option. If theres 2 baseA and 3 baseB, 1 = A, 4 = B.
-            eyeNum = random.randint(1,eyeACount)            # Uses a random eye (between 1 and the highest eye number in the folder)
-            patternNum = random.randint(1,patternACount)
-            
-            baseFile = "baseA" + str(baseNum) + ".png"          # Finds the right base file path name
-            eyeFile = "eyesA" + str(eyeNum) + ".png"            # Finds the right eye file path name
-            patternFile = "patternA" + str(patternNum) + ".png" # Finds the right pattern file path name
+        if variantNumber == 1: # Treats base A as the starting option. If theres 2 baseA and 3 baseB, 1 = A, 4 = B.
+            baseNum = random.randint(1,12)
+            eyeNum = random.randint(1,(eyeCount/2))            # Uses a random eye (between 1 and the highest eye number in the folder)
+            patternNum = random.randint(1,2) 
+            baseFile = basePath + "baseA" + str(baseNum) + ".png"          # Finds the right base file path name
+            eyeFile = eyePath + "eyeA" + str(eyeNum) + ".png"            # Finds the right eye file path name
+            patternFile = patternPath + "patternA" + str(patternNum) + ".png" # Finds the right pattern file path name
             print("[INFO] Files to be combined are " + str(baseFile) + ", " + str(eyeFile) + " and " + str(patternFile) + ".")
             saveFile = str(batchName) + "_worm_A_b" + str(baseNum) + "_e" + str(eyeNum) + "_p" + str(patternNum) + ".png" # Figures out where to save the generated worm image
             savePath = os.path.join(outputFolder, saveFile)
@@ -86,14 +69,14 @@ while not done:
                 continue
 
         # ======== Base B ======== #
-        elif (baseNum > baseACount) & (baseNum <= baseCount):                           # Treats base A as the starting option. If theres 2 baseA and 3 baseB, 1 = A, 4 = B.
-            eyeNum = random.randint(1,eyeBCount)           # Uses a random eye (between 1 and the highest eye number in the folder)
-            patternNum = random.randint(1,patternBCount)
-            baseNum = (baseNum-baseACount)                 # Needs to be added, otherwise it would be too high
+        elif variantNumber == 2:                           # Treats base A as the starting option. If theres 2 baseA and 3 baseB, 1 = A, 4 = B.
+            baseNum = random.randint(1,12)
+            eyeNum = random.randint(1,(eyeCount/2))           # Uses a random eye (between 1 and the highest eye number in the folder)
+            patternNum = random.randint(1,2)
             
-            baseFile = "baseB" + str(baseNum) + ".png"     # Finds the right base file path name
-            eyeFile = "eyesB" + str(eyeNum) + ".png"                    # Finds the right eye file path name
-            patternFile = "patternB" + str(patternNum) + ".png"         # Finds the right pattern file name
+            baseFile = basePath + "baseB" + str(baseNum) + ".png"          # Finds the right base file path name
+            eyeFile = eyePath + "eyeB" + str(eyeNum) + ".png"            # Finds the right eye file path name
+            patternFile = patternPath + "patternB" + str(patternNum) + ".png" # Finds the right pattern file path name
             print("[INFO] Files to be combined are " + str(baseFile) + ", " + str(eyeFile) + " and " + str(patternFile) + ".")
 
             saveFile = str(batchName) + "_worm_B_b" + str(baseNum) + "_e" + str(eyeNum) + "_p" + str(patternNum) + ".png" # Figures out where to save the generated worm image
