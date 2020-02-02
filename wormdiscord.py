f4f564e1c7db68e226347391100c43d3b7c7b170
therats-collab <linuxfan413@pm.me>
2020-02-02 17:52:27 +1030

    Basic bot function

--- wormdiscord.py
+++ wormdiscord.py
@@ -11,7 +11,7 @@
 # === Imports === #
 
 import os
-
+import random
 import discord
 from dotenv import load_dotenv
 
@@ -35,6 +35,29 @@ async def on_ready():
 
     members = '\n - '.join([member.name for member in guild.members])
     print(f'[INFO] Guild Members:\n - {members}')
+
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    random_messages = [
+        'I declare you 99% valid.',
+        'Do you ever just...scream into the void',
+    ]
+
+    if message.content == '$ test':
+        response = random.choice(random_messages)
+        await message.channel.send(response)
 
 
 client.run(TOKEN)
